4ae435cf1607cf6de94b951006a6284b736f59a5
fireman9 <nazariystr@gmail.com>
2021-09-20 19:41:59 +0300

    added bfs for ghosts using

--- Algorithms.h
+++ Algorithms.h
@@ -16,6 +16,9 @@ public:
 
     vector<pair<int, int>> bfs(int startX, int startY, int destX, int destY, vector<vector<int>> &map);
 
+    vector<pair<int, int>>
+    bfsForGhosts(int startX, int startY, int destX, int destY, vector<vector<int>> &map, int direction);
+
     vector<pair<int, int>> ucs(int startX, int startY, int destX, int destY, vector<vector<int>> &map);
 
     vector<pair<int, int>> aStar(int startX, int startY, int destX, int destY, vector<vector<int>> &map);
